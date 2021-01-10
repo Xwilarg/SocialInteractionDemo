8d71e7921d905884a00e263c90d25ccb67c37084
Xwilarg <xwilarg@yahoo.fr>
2021-01-10 17:14:38 +0100

    Add FPS controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private const float _speed = 10f;
+    private const float _sensibilityX = 2f;
+    private const float _sensibilityY = 2f;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        int x = 0, y = 0;
+        if (Input.GetKey(KeyCode.Z)) y = 1;
+        else if (Input.GetKey(KeyCode.S)) y = -1;
+        if (Input.GetKey(KeyCode.Q)) x = -1;
+        else if (Input.GetKey(KeyCode.D)) x = 1;
+        var axis2D = (new Vector2(transform.forward.x, transform.forward.z) * y) + (new Vector2(transform.right.x, transform.right.z) * x);
+        axis2D.Normalize();
+        _rb.velocity = new Vector3(axis2D.x, _rb.velocity.y, axis2D.y);
+
+        float rotX = Input.GetAxis("Mouse X");
+        float rotY = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotX * Vector3.up * _sensibilityY);
+        Camera.main.transform.Rotate(rotY * Vector3.left * _sensibilityX);
+    }
+}
