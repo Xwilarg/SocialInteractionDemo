fa7ee7e515729daad1a04525583283691d867c00
Xwilarg <xwilarg@yahoo.fr>
2021-01-12 21:35:36 +0100

    Add AIs

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _player;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(_player.transform.position);
+    }
+}
