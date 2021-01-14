1bca478fc61d519d21f062f4f63dc826511b2228
Xwilarg <xwilarg@yahoo.fr>
2021-01-14 12:08:56 +0100

    Add gamepad controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
     private const float _speed = 10f;
-    private const float _sensibilityX = 2f;
-    private const float _sensibilityY = 2f;
+    private const float _sensibilityX = 5f;
 
     private const float _shootForce = 50f;
 
@@ -41,26 +40,21 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        int x = 0, y = 0;
-        if (Input.GetKey(KeyCode.Z)) y = 1;
-        else if (Input.GetKey(KeyCode.S)) y = -1;
-        if (Input.GetKey(KeyCode.Q)) x = -1;
-        else if (Input.GetKey(KeyCode.D)) x = 1;
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
         var axis2D = (new Vector2(transform.forward.x, transform.forward.z) * y) + (new Vector2(transform.right.x, transform.right.z) * x);
         axis2D.Normalize();
         _rb.velocity = new Vector3(axis2D.x * _speed, _rb.velocity.y, axis2D.y * _speed);
 
-        float rotX = Input.GetAxis("Mouse X");
-        float rotY = Input.GetAxis("Mouse Y");
-        transform.Rotate(rotX * Vector3.up * _sensibilityY);
-        Camera.main.transform.Rotate(rotY * Vector3.left * _sensibilityX);
+        float rotX = Input.GetAxis("RotateX");
+        transform.Rotate(rotX * Vector3.up * _sensibilityX);
     }
 
     private void Update()
     {
         _timerBetweenShoot -= Time.deltaTime;
 
-        if (Input.GetMouseButtonDown(0) && _timerBetweenShoot <= 0f && !_isReloading)
+        if (Input.GetButtonDown("Fire1") && _timerBetweenShoot <= 0f && !_isReloading)
         {
             var bullet = Instantiate(_bulletPrefab, _gunEnd.transform.position, Quaternion.identity);
             bullet.GetComponent<Rigidbody>().AddForce(transform.forward * _shootForce, ForceMode.Impulse);
